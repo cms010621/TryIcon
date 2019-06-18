e4eac7c07abad474dfcefb7f194920f3068f9aec
Minsung Choi <cms010621@gmail.com>
2019-06-18 15:02:15 +0900

    #3 트레이아이콘 코드 구현 by 3216 최민성

--- TryIcon/Form1.cs
+++ TryIcon/Form1.cs
@@ -15,5 +15,38 @@ namespace TryIcon
         {
             InitializeComponent();
         }
+        private void VisibleChange(bool visible)
+        {
+            this.Visible = visible;
+            this.ntiTray.Visible = !visible;
+        }
+
+        private void BtnTray_Click(object sender, EventArgs e)
+        {
+            VisibleChange(false); //버튼을 누르면 사라져야한다.
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true; //닫기버튼 눌러도 종료되지 않음.
+            VisibleChange(false);
+        }
+
+        private void NtiTray_DoubleClick(object sender, EventArgs e)
+        {
+            VisibleChange(true);
+        }
+
+        private void 폼보이기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            VisibleChange(true);
+        }
+
+        private void 종료ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.ntiTray.Visible = false;
+            Application.ExitThread(); //프로그램 완전종료
+        }
     }
 }
